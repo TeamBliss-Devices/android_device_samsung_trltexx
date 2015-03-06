f6233624501706bc714feb712f237e857a853ca0
remicks <lost.junglist@gmail.com>
2015-03-05 22:05:49 -0600

    Blissify
    We are Bliss.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,3 +44,12 @@ BOARD_RIL_CLASS := ../../../device/samsung/trltexx/ril
 
 # inherit from the proprietary version
 -include vendor/samsung/trltexx/BoardConfigVendor.mk
+
+# BlissPop Config Flags
+TARGET_TC_ROM := 4.8
+TARGET_TC_KERNEL := 4.10-linaro
+BLISSIFY := true
+BLISS_O3 := true
+BLISS_STRICT := true
+BLISS_GRAPHITE := true
+BLISS_KRAIT := false
